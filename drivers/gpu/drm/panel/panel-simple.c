94bf587495257614eb01dfa4f2cf2959635b1e0e
David Brandt <d.brandt@phytec.de>
2019-04-05 09:51:04 +0200

    drm/panel: simple: Add support for Tianma TM070JDHG30
    The Tianma TM070JVHG33 is a 7" panel with a 1280x800 resolution
    and connected to LVDS using four lanes. Values from datasheet
    r1.3 (2017-3-9) used.
    
    Signed-off-by: David Brandt <d.brandt@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -1895,6 +1895,31 @@ static const struct panel_desc tianma_tm070jdhg30 = {
 	.bus_format = MEDIA_BUS_FMT_RGB888_1X7X4_SPWG,
 };
 
+static const struct drm_display_mode tianma_tm070jvhg33_timing = {
+	.clock = 68000,
+	.hdisplay = 1280,
+	.hsync_start = 1280 + 64,
+	.hsync_end = 1280 + 64 + 5,
+	.htotal = 1280 + 64 + 5 + 10,
+	.vdisplay = 800,
+	.vsync_start = 800 + 40,
+	.vsync_end = 800 + 40 + 2,
+	.vtotal = 800 + 40 + 2 + 10,
+	.vrefresh = 60,
+};
+
+static const struct panel_desc tianma_tm070jvhg33 = {
+	.modes = &tianma_tm070jvhg33_timing,
+	.num_modes = 1,
+	.bpc = 8,
+	.size = {
+		.width = 151,
+		.height = 95,
+	},
+	.bus_format = MEDIA_BUS_FMT_RGB888_1X24,
+	.bus_flags = DRM_BUS_FLAG_DE_LOW,
+};
+
 static const struct drm_display_mode tpk_f07a_0102_mode = {
 	.clock = 33260,
 	.hdisplay = 800,
@@ -2182,6 +2207,9 @@ static const struct of_device_id platform_of_match[] = {
 	}, {
 		.compatible = "tianma,tm070jdhg30",
 		.data = &tianma_tm070jdhg30,
+	}, {
+		.compatible = "tianma,tm070jvhg33",
+		.data = &tianma_tm070jvhg33,
 	}, {
 		.compatible = "tpk,f07a-0102",
 		.data = &tpk_f07a_0102,
