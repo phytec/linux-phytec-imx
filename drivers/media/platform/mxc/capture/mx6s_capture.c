5c251cd3a9b3f0798c8fdcd2af7376b5fce7248b
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2019-10-18 08:48:57 +0200

    media:mx6s_capture: fixed NULL pointer access
    Signed-off-by: Enrico Scholz <enrico.scholz@sigma-chemnitz.de>

--- drivers/media/platform/mxc/capture/mx6s_capture.c
+++ drivers/media/platform/mxc/capture/mx6s_capture.c
@@ -1637,7 +1637,7 @@ static int mx6s_vidioc_enum_framesizes(struct file *file, void *priv,
 	int ret;
 
 	fmt = format_by_fourcc(fsize->pixel_format);
-	if (fmt->pixelformat != fsize->pixel_format)
+	if (!fmt || fmt->pixelformat != fsize->pixel_format)
 		return -EINVAL;
 	fse.code = fmt->mbus_code;
 
@@ -1679,7 +1679,7 @@ static int mx6s_vidioc_enum_frameintervals(struct file *file, void *priv,
 	int ret;
 
 	fmt = format_by_fourcc(interval->pixel_format);
-	if (fmt->pixelformat != interval->pixel_format)
+	if (!fmt || fmt->pixelformat != interval->pixel_format)
 		return -EINVAL;
 	fie.code = fmt->mbus_code;
 
