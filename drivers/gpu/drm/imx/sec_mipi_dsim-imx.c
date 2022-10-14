693debc9432624cc41fb4412483abb71516b23ae
Teresa Remmet <t.remmet@phytec.de>
2022-10-14 14:40:24 +0200

    drm: imx: sec_mipi_dsim-imx: Fix error handling of probe
    Clean up probe when component_add() returns an error.
    Otherwise deferred probe in component_add() leads to driver load
    failure and back traces during the second try.
    
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- drivers/gpu/drm/imx/sec_mipi_dsim-imx.c
+++ drivers/gpu/drm/imx/sec_mipi_dsim-imx.c
@@ -456,7 +456,14 @@ static int imx_sec_dsim_probe(struct platform_device *pdev)
 
 	pm_runtime_enable(dev);
 
-	return component_add(dev, &imx_sec_dsim_ops);
+	ret = component_add(dev, &imx_sec_dsim_ops);
+	if (ret) {
+		pm_runtime_disable(&pdev->dev);
+		sec_dsim_of_put_resets(dsim_dev);
+		return ret;
+	}
+
+	return 0;
 }
 
 static int imx_sec_dsim_remove(struct platform_device *pdev)
