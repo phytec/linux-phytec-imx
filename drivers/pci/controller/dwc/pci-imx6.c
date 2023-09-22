9e73dc14e08c718a8534588a29c004e2bc1b1f4e
Yannic Moog <y.moog@phytec.de>
2023-09-22 08:20:16 +0200

    BSPIMX8M-2876 pci: imx: remove epdev_on regulator
    commit d3c633e5cab3 ("MLK-15141-1: PCI: imx: Add epdev_on regulator for
    8QM WiFi") introduced code that adds a regulator independently of the
    underlying hardware, i.e. it tried to read a device tree property that
    is and should not be present on all imx SoCs but required it for all.
    This lead to warning messages that have no relevance.
    Remove the code because we do not support the 8QM.
    
    Also remove a code fragment introduced in 02009b920180 ("LF-128 PCI:
    imx: turn off the clocks and regulators when link is down") that
    relates to the "epdev_on" regulator.
    
    Signed-off-by: Yannic Moog <y.moog@phytec.de>
    Tested-by: Yashwanth Varakala <y.varakala@phytec.de>
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>

--- drivers/pci/controller/dwc/pci-imx6.c
+++ drivers/pci/controller/dwc/pci-imx6.c
@@ -162,7 +162,6 @@ struct imx6_pcie {
 	struct device_link	*pd_hsio_link;
 
 	const struct imx6_pcie_drvdata *drvdata;
-	struct regulator	*epdev_on;
 	struct phy		*phy;
 };
 
@@ -1893,8 +1892,6 @@ err_reset_phy:
 		imx6_pcie_clk_disable(imx6_pcie);
 		if (imx6_pcie->vpcie != NULL)
 			regulator_disable(imx6_pcie->vpcie);
-		if (imx6_pcie->epdev_on != NULL)
-			regulator_disable(imx6_pcie->epdev_on);
 		if (gpio_is_valid(imx6_pcie->dis_gpio))
 			gpio_set_value_cansleep(imx6_pcie->dis_gpio, 0);
 	}
@@ -2432,9 +2429,7 @@ static int imx6_pcie_probe(struct platform_device *pdev)
 	} else if (imx6_pcie->dis_gpio == -EPROBE_DEFER) {
 		return imx6_pcie->dis_gpio;
 	}
-	imx6_pcie->epdev_on = devm_regulator_get(&pdev->dev, "epdev_on");
-	if (IS_ERR(imx6_pcie->epdev_on))
-		return -EPROBE_DEFER;
+
 	imx6_pcie->reset_gpio = of_get_named_gpio(node, "reset-gpio", 0);
 	imx6_pcie->gpio_active_high = of_property_read_bool(node,
 						"reset-gpio-active-high");
@@ -2650,12 +2645,6 @@ static int imx6_pcie_probe(struct platform_device *pdev)
 	ret = imx6_pcie_attach_pd(dev);
 	if (ret)
 		return ret;
-
-	ret = regulator_enable(imx6_pcie->epdev_on);
-	if (ret) {
-		dev_err(dev, "failed to enable the epdev_on regulator\n");
-		goto err_ret;
-	}
 
 	if (gpio_is_valid(imx6_pcie->dis_gpio))
 		gpio_set_value_cansleep(imx6_pcie->dis_gpio, 1);
