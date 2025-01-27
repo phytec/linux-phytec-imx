3890ea5e5c8b53a6f471e2f9dbc602bd26893d9f
Felix Siebel <f.siebel@phytec.de>
2025-01-27 14:20:13 +0100

    BSPIMX8M-2783 iio: industrial IO core: add new flags
    Added new Flag 'IIO_CHAN_REF_SOURCE' and 'IIO_CHAN_REF_VOLTAGE'
    used by ADS1119 driver
    
    Signed-off-by: Felix Siebel <f.siebel@phytec.de>
    Reviewed-by: Stefan Riedmueller <s.riedmueller@phytec.de>
    (cherry picked from commit f7d8b9fd39a9d00e6e8dbbb4279929bef1d9f747)
    Signed-off-by: Yannic Moog <y.moog@phytec.de>
    Signed-off-by: Teresa Remmet <t.remmet@phytec.de>
    (cherry-picked from 4fd2369734387988b66ce48d37c4fb93984f54b7)
    Signed-off-by: Andrej Picej <andrej.picej@norik.com>
    Signed-off-by: Christian Hemp <c.hemp@phytec.de>

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -178,6 +178,8 @@ static const char * const iio_chan_info_postfix[] = {
 	[IIO_CHAN_INFO_THERMOCOUPLE_TYPE] = "thermocouple_type",
 	[IIO_CHAN_INFO_CALIBAMBIENT] = "calibambient",
 	[IIO_CHAN_INFO_ZEROPOINT] = "zeropoint",
+	[IIO_CHAN_INFO_REFSOURCE] = "reference_source",
+	[IIO_CHAN_INFO_REFVOLTAGE] = "reference_voltage",
 };
 /**
  * iio_device_id() - query the unique ID for the device
